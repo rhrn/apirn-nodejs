8423ba22c77767c6e9c85386547a4cfe860d8d96
Nesterov Roman <nester@bk.ru>
2013-04-01 18:29:30 +0400

    init application cache

--- routers/web.js
+++ routers/web.js
@@ -1,7 +1,21 @@
 var auth = require('../controllers/auth.js'),
-  index = require('../controllers/index.js');
+  index = require('../controllers/index.js'),
+  fs = require('fs');
 
 module.exports = function(app) {
+
+  app.get('/cache.manifest', function(req, res) {
+
+    res.header("Content-Type", "text/cache-manifest");
+    res.header("Cache-Control", "no-cache, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", "-1");
+
+    fs.readFile('appcache/cache.manifest', 'utf8', function(err, data) {
+      res.send(data);
+    });
+
+  });
 
   app.get('/', index.index);
   app.post('/', index.index);
