88babe95518e909e3363498e882d141c8bb7e8eb
Nesterov Roman <nester@bk.ru>
2013-05-15 19:33:42 +0400

    fixup! attempt to enable part content

--- controllers/api/files.js
+++ controllers/api/files.js
@@ -1,6 +1,7 @@
 var files = require('../../models/user.files.js');
 
 var utils = {
+
   range: function(range) {
 
     console.log('utils range', range);
@@ -13,9 +14,10 @@ var utils = {
 
       data.start = parseInt(range[1], 10);
 
-      if (range[2] === undefined) {
-        range[2] = data.start + 1024;
+      if (range[2] !== undefined) {
+        range[2] = parseInt(range[2], 10);
       }
+
       data.end = range[2];
     }
 
@@ -46,25 +48,31 @@ module.exports = {
 
   download: function(req, res) {
 
+    var status = 200;
+
+    if (req.headers.range) {
+      status = 206;
+    }
+
     var range = utils.range(req.headers.range);
 
     //files.private(req.cookies.token, req.params.id, function(file, data, user) {
     files.public(req.params.id, range, function(file, data) {
+ 
+      file.length = file.size - range.start;
+      range.end = file.size - 1;
 
-      file.length = file.length || file.size;
-      range.end = range.end || (range.start + file.length - 1) || file.size - 1;
+      res.setHeader('Accept-Ranges', 'bytes');
 
       res.setHeader('Content-Type', file.type);
-      res.setHeader('Content-Length', file.size);
+      res.setHeader('Content-Length', file.length);
       res.setHeader('Content-Disposition', 'inline; filename="' + file.name + '"');
 
-      res.setHeader('Content-Transfer-Encoding', 'binary');
       res.setHeader('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + file.size);
-      res.setHeader('Accept-Ranges', 'bytes');
 
       console.log('header', res.getHeader('Content-Range'));
 
-      res.send(206, data);
+      res.send(status, data);
 
     });
 
